4134613f2dc7a1c4bc7003a4ddb4ec916ce7f817
zhangguobin1994 <982415446@qq.com>
2019-07-02 14:55:09 +0800

    初始提交

---
+++ demo01/src/main/java/text/A.java
@@ -0,0 +1,14 @@
+package text;
+
+public class A {
+
+    public static void main(String[] args) {
+      ///////
+        User u = new User();
+        u.setName("jack");
+        u.setAge(18);
+        System.out.println(u);
+
+    }
+
+}
