b88ec425cad9bdd4b5d3e82b6c1ab958a7cfe93c
zhangguobin1994 <982415446@qq.com>
2019-07-02 14:59:49 +0800

    4

--- demo01/src/main/java/text/A.java
+++ demo01/src/main/java/text/A.java
@@ -3,7 +3,7 @@ package text;
 public class A {
 
     public static void main(String[] args) {
-      //////////////////
+      /////////////////////
         User u = new User();
         u.setName("jack");
         u.setAge(18);
